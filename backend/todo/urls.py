5425b83881dd990fd2e38d7b73531908d90fed8b
Michal <armonmic22003@gmail.com>
2026-06-15 19:34:30 +0300

    ב

--- backend/todo/urls.py
+++ backend/todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import task_list
+from .views import task_list, toggle_task
 
 urlpatterns = [
     path("", task_list, name="task_list"),
+    path("toggle/<int:task_id>/", toggle_task, name="toggle_task"),
 ]
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
 
@@ -14,3 +14,12 @@ def task_list(request):
     tasks = Task.objects.all().order_by("-created_at")
 
     return render(request, "todo/task_list.html", {"tasks": tasks})
+
+
+def toggle_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+
+    task.completed = not task.completed
+    task.save()
+
+    return redirect("task_list")
