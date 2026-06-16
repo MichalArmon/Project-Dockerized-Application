35e961156a0be01ac076d1136e82cb9a8846799f
Michal <armonmic22003@gmail.com>
2026-06-16 18:34:34 +0300

    ב

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
 
 from .models import Task
 from .serializers import TaskSerializer
@@ -7,3 +8,5 @@ from .serializers import TaskSerializer
 class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all().order_by("-created_at")
     serializer_class = TaskSerializer
+    authentication_classes = []
+    permission_classes = [AllowAny]
